35ffc171290701d36920a5ec03521b3819a95268
undefined <akzholbusiness@gmail.com>
2021-11-21 23:24:58 +0600

    hosted postgres database on heroku

--- src/com/company/Model/DB/PostgresAdapter.java
+++ src/com/company/Model/DB/PostgresAdapter.java
@@ -21,10 +21,10 @@ public class PostgresAdapter implements IPostgresAdapter{
 
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
-        String connectionURL = "jdbc:postgresql://localhost:5432/BettingSystem";
+        String connectionURL = "jdbc:postgresql://ec2-34-194-119-178.compute-1.amazonaws.com:5432/d6i0u20epksrj0";
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(connectionURL, "postgres", "1234");
+            return DriverManager.getConnection(connectionURL, "glpeslkjefmldw", "2ff26e34d362e4066a37f620bb299d0f9b00d1efd5a1c274c30ab43ebd8b5f4e");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw e;
